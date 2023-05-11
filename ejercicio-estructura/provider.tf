9ca1c5eac1313eeb3fd571ead022c3367375a495
Miguel Leiva <migueleiva@gmail.com>
2023-05-11 00:41:45 -0500

    Init proyect

---
+++ ejercicio-estructura/provider.tf
@@ -0,0 +1,9 @@
+//Brain Tech
+//Ejercicio inicial
+//Captura las credenciales del archivo config
+provider "aws" {
+  //profile    = "default"
+  region     = "us-east-1"
+  access_key = "AKIA2EG4FN553DZN2LDO"
+  secret_key = "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"
+}
